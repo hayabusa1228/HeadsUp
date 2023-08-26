db14118bc164de8dac5d8c84d586a438376ddd44
hayabusa1228 <hayato@DESKTOP-HH8F120>
2023-08-26 21:44:03 +0900

    eiteeewewwweer push

--- server/server.py
+++ server/server.py
@@ -341,4 +341,5 @@ if __name__ == "__main__":
  with app.app_context():
     db.create_all()
 #   app.run(host="127.0.0.1",debug=True,port=5000)
- socketio.run(app,host='0.0.0.0',port=5000,debug=True)
+#  socketio.run(app,host='0.0.0.0',port=5000,debug=True)
+ socketio.run(app,host='0.0.0.0',port=5000)
